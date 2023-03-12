1561a9e243f644f09e7ad4bcbedd4f4d7b067fdb
test <legoupil.william@gmail.com>
2023-03-12 21:51:33 +0100

    accueil

--- app_dynamics/test/widget_test.dart
+++ app_dynamics/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:app_dynamics/main.dart';
+import 'package:App_dynamics/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
